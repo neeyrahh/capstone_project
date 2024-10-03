8977899b70b9947521aa3f766b26979f6ba9472b
neeyrahh <84280080+neeyrahh@users.noreply.github.com>
2024-10-03 14:08:48 -0400

    fixed changes on dashboard

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,6 +4,13 @@ import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
 // import { useEmployeeData } from "./EmployeeData";
 
 const COLORS = ['#2996ff', '#e8651d', '#ff0000b3', '#861de8e3', '#9a1de8'];
+const data = [
+  { name: "Total Task", value: 30 },
+  { name: "Started", value: 20 },
+  { name: "In-Progress", value: 25 },
+  { name: "Done", value: 15 },
+
+];
 
 const Homepage = () => {
   // const { loading, error, data } = useEmployeeData();
@@ -33,7 +40,7 @@ const Homepage = () => {
                 <div className="col-md-3 mb-3 card-body d-flex flex-column align-items-left">
                   <div className="card shadow-sm h-100 d-flex flex-column justify-content-center align-items-center" style={{ backgroundColor: '#1d84e8b3', color: '#ffffff', border:'none' }}>
                     <div className="card-body text-center">
-                      <h6 className="card-title">Total Employees</h6>
+                      <h6 className="card-title">Total Task</h6>
                       {/* <p className="stat-val">{data.totalEmployees}</p> */}
                       <p className="stat-val">2</p>
                     </div>
@@ -42,7 +49,7 @@ const Homepage = () => {
                 <div className="col-md-3 mb-3 card-body d-flex flex-column align-items-left">
                   <div className="card shadow-sm h-100 d-flex flex-column justify-content-center align-items-center" style={{ backgroundColor: '#e8651dba', color: '#ffffff', border:'none' }}>
                     <div className="card-body text-center">
-                      <h6 className="card-title">Departments</h6>
+                      <h6 className="card-title">Started</h6>
                       <p className="stat-val">2</p>
                     </div>
                   </div>
@@ -50,7 +57,7 @@ const Homepage = () => {
                 <div className="col-md-3 mb-3">
                   <div className="card shadow-sm h-100 d-flex flex-column justify-content-center align-items-center" style={{ backgroundColor: '#ff00008f', color: '#ffffff', border:'none' }}>
                     <div className="card-body text-center">
-                      <h6 className="card-title">Upcoming Retirements</h6>
+                      <h6 className="card-title">Inprogress</h6>
                       <p className="stat-val">2</p>
                     </div>
                   </div>
@@ -58,7 +65,7 @@ const Homepage = () => {
                 <div className="col-md-3 mb-3">
                   <div className="card shadow-sm h-100 d-flex flex-column justify-content-center align-items-center" style={{ backgroundColor: '#861de8a6', color: '#ffffff', border:'none' }}>
                     <div className="card-body text-center">
-                      <h6 className="card-title">Active Employees</h6>
+                      <h6 className="card-title">Done</h6>
                       <p className="stat-val">2</p>
                     </div>
                   </div>
@@ -71,10 +78,10 @@ const Homepage = () => {
         <div className="col-md-6 d-flex mb-6">
           <div className="card shadow-sm flex-fill" style={{ backgroundColor: '#b4b4b41c', color: '#212529', border:'none' }}>
             <div className="card-body d-flex flex-column justify-content-center align-items-center">
-              <h5 className="card-title text-left">Employee Distribution by Department</h5>
+              <h5 className="card-title text-left">Task Distribution</h5>
               <PieChart width={400} height={300}>
                 <Pie
-                  // data={data.departmentDistribution}
+                  data={data}
                   cx={200}
                   cy={120}
                   stroke="0"
@@ -87,6 +94,9 @@ const Homepage = () => {
                   {/* {data.departmentDistribution.map((entry, index) => (
                     <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                   ))} */}
+                   {COLORS.map((color, index) => (
+                    <Cell key={`cell-${index}`} fill={color} />
+                  ))}
                 </Pie>
                 <Tooltip />
                 <Legend />
